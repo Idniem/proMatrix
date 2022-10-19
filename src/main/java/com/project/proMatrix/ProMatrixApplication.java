b6aa3a678eae9d1bd0bdb1e97c171753f390be87
idniem <dariusz.niem@outlook.com>
2022-10-19 22:59:54 +0200

    Project added

---
+++ src/main/java/com/project/proMatrix/ProMatrixApplication.java
@@ -0,0 +1,13 @@
+package com.project.proMatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProMatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProMatrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/proMatrix/ProMatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.proMatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProMatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
